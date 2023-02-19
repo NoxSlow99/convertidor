fed194230b829ce013d356cae321a9764525fc10
NoxSlow99 <angel_jcc99@outlook.com>
2023-02-19 14:53:32 -0500

    Iniciando proyecto

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+package main;
+
+import views.MenuView;
+
+/**
+ *
+ * @author angel
+ */
+public class main {
+
+    public static void main(String[] args) {
+        MenuView menu = new MenuView();
+        menu.setVisible(true);
+    }
+    
+}
